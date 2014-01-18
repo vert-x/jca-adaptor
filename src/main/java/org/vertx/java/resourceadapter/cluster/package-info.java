fc777f50b5c2ff6408a736877d1c17a4494774fa
Lin Gao <aoingl@gmail.com>
2014-01-18 20:14:48 +0800

    Removed claster manager implementation, use the vertx one instead.
    See PR: https://github.com/eclipse/vert.x/pull/759

--- src/main/java/org/vertx/java/resourceadapter/cluster/package-info.java
+++
@@ -1,4 +0,0 @@
-
-// The vertx-hazelcast needs to be fixed to be able to use different cluster file than cluster.xml/default-cluster.xml at runtime.
-@java.lang.Deprecated
-package org.vertx.java.resourceadapter.cluster;
